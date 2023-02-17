88bc81c0b17e99d74c3fa2015888ffae998adcc9
sangood <zhangsangood@gmail.com>
2023-02-17 19:01:42 +0800

    Update diy-part1.sh
    echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
